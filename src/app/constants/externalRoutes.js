8588232a92e5dddf03d04a27ea74325953f39b9d
Jordaniel Wolk <jd@philosophie.is>
2017-02-08 12:29:43 -0800

    Adds social login buttons

--- src/app/constants/externalRoutes.js
+++ src/app/constants/externalRoutes.js
@@ -4,19 +4,20 @@ const ExternalRoutes = () => {
   const { backendUrl } = Paths
 
   const routes = {
-    bookStore: () => backendUrl('browse'),
     about: () => backendUrl('aboutus-maybe'),
-    news: () => backendUrl('news'),
+    advertisers: () => backendUrl('advertising'),
     articles: () => backendUrl('articles'),
     authors: () => backendUrl('authors-maybe'),
-    support: () => 'https://support.readerslegacy.com/', // TODO: until further notice
-    privacy: () => backendUrl('privacy'),
-    terms: () => backendUrl('terms'),
-    publishers: () => backendUrl('publishers_enrollment'),
-    advertisers: () => backendUrl('advertising'),
-    media: () => backendUrl('media-page'),
+    bookStore: () => backendUrl('browse'),
     childrensLiteracy: () => backendUrl('childrens-literacy'),
-    litcoins: () => backendUrl('litcoins')
+    litcoins: () => backendUrl('litcoins'),
+    media: () => backendUrl('media-page'),
+    news: () => backendUrl('news'),
+    privacy: () => backendUrl('privacy'),
+    providerLogin: ({ provider }) => backendUrl(`accounts/${provider}/login`),
+    publishers: () => backendUrl('publishers_enrollment'),
+    support: () => 'https://support.readerslegacy.com/', // TODO: until further notice
+    terms: () => backendUrl('terms'),
   }
   return routes
 }
