31c4626ed1c9da480502cbce0c5f3bae527af034
Carlos Sampol <sampol.90@gmail.com>
2017-02-09 09:41:49 -0500

    Removing back button in step 1

--- src/app/components/readerOnboarding/SignUpButtons.js
+++ src/app/components/readerOnboarding/SignUpButtons.js
@@ -15,6 +15,24 @@ const SignUpButtons = ({ stepIndex, handleButtonClick, disabled, buttonType }) =
   const isFinished = (stepIndex === Steps.last()) ? 'Finish' : 'Next'
   const isDisabled = stepIndex === Steps.first()
 
+  console.log(`The step index is ${stepIndex}`)
+
+  if (stepIndex === 0) {
+    return (
+      <div style={{ marginTop: 50, marginBottom: 12 }}>
+        <div style={styles.nextButton}>
+          <PrimaryButton
+            label={isFinished}
+            value={isFinished}
+            disabled={disabled ? disabled : false}
+            primary={true}
+            type={buttonType === 'form' ? 'submit' : null}
+            onClick={buttonType === 'form' ? null : handleButtonClick}
+          />
+        </div>
+      </div>
+    )
+  }
   return (
     <div style={{ marginTop: 50, marginBottom: 12 }}>
       <div style={styles.backButton}>
