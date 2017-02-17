5e0fd0bf50836b1bd2afa8650c1d6e832d0aeac0
Mashadim <masha@philosophie.is>
2017-02-17 12:41:49 -0800

    Fixed validation errors render on Step 0 and input bug on Step 1

--- src/app/services/queryParams.js
+++ src/app/services/queryParams.js
@@ -6,14 +6,8 @@ const QueryParams = () => {
     if (R.isEmpty(paramsObj)) {
       return ''
     }
-    // NOTE: API needs array params of the format ?arg=X,Y,Z which isn't
-    // commonly supported. Need to handle them separately from other
-    // types of params
-    const [arrayArgs, otherArgs] = R.partition(Array.isArray, paramsObj)
-    const arraysAsStrings = R.map(R.join(','), arrayArgs)
-    const allArgs = R.merge(otherArgs, arraysAsStrings)
 
-    return `?${QS.stringify(allArgs, { encode: false })}` //temporary
+    return `?${QS.stringify(paramsObj)}`
   }
 
   return {
