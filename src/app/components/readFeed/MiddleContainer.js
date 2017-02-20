af950ede50abf12b52aa86fde607e642af68544d
Emir Salazar <emir@nextcenturypublishing.com>
2017-02-20 10:02:31 -0500

    Adding backbone structure for the readfeed columnnary grid.

---
+++ src/app/components/readFeed/MiddleContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MiddleContainer = () => {
+  return (
+    <div className='middle-container small-12 medium-6 columns'>
+      <p>Middle Container</p>
+    </div>
+  )
+}
+
+export default MiddleContainer
