ddd49ac996651721786a9d0a22d9c39fd9598166
Emir Salazar <emir@nextcenturypublishing.com>
2017-02-21 10:15:32 -0500

    Adding logic to truncate shown book information on GoRead Landing's Books.

--- src/app/components/home/Book.js
+++ src/app/components/home/Book.js
@@ -6,20 +6,35 @@ const styles = {
     textAlign: 'center',
   },
 }
+const bookInfoCharLimit = 40
 
 const Book = ({ book }) => {
+  const truncInfo = (text, limit) => {
+    return text.length > limit ? `${text.slice(0, limit)}...` : null
+  }
+  const renderedText = truncInfo(`${book.title} by ${book.author}`, bookInfoCharLimit)
+
   return (
     <div style={styles.bookSection}>
       <div style={styles.bookImage} className='book-container'>
         <a href={book.slug}>
-        <div className='book-info'>
-          <span className='link'>
-            {book.title}
-          </span><br />
-          <span className='link'>
-            by {book.author}
-          </span>
-        </div>
+          { renderedText != null ? (
+              <div className='book-info'>
+                <span className='link'>
+                  {renderedText}
+                </span>
+              </div>
+            ) : (
+              <div className='book-info'>
+                <span className='link'>
+                  {book.title}
+                </span><br />
+                <span className='link'>
+                  by {book.author}
+                </span>
+              </div>
+            )
+          }
           <img className='book' src={book.imageUrl} />
         </a>
       </div>
