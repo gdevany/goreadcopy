ea5617a5145cd6817d5bbf0dc535b6cd4ac68c5c
Carlos Sampol <sampol.90@gmail.com>
2017-03-07 10:55:54 -0500

    Creating and styling right sidebar advertising card

--- src/app/components/readFeed/RightContainer.js
+++ src/app/components/readFeed/RightContainer.js
@@ -2,10 +2,12 @@ import React from 'react'
 import BookRecommendations from './BookRecommendations'
 import AuthorRecommendations from './AuthorRecommendations'
 import BookClubRecommendations from './BookClubRecommendations'
+import SidebarAd from './SidebarAd'
 
 const RightContainer = () => {
   return (
     <div className='right-container small-12 large-3 columns'>
+      <SidebarAd />
       <BookRecommendations />
       <AuthorRecommendations />
       <BookClubRecommendations />
