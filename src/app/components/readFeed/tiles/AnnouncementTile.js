466ca7ac3880436d7937e78d01dbc48072a82619
Carlos Sampol <sampol.90@gmail.com>
2017-03-08 14:44:22 -0500

    Adding styles to announcement on readfeed

--- src/app/components/readFeed/tiles/AnnouncementTile.js
+++ src/app/components/readFeed/tiles/AnnouncementTile.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react'
+import {
+  Card,
+  CardText,
+} from 'material-ui'
 
 class AnnouncementTile extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      isOpen: '',
       title: 'Welcome to GoRead!',
-      body: `This is your Read Feed. You\'ll find 
-             books and content from Authors and 
+      body: `This is your Read Feed. You\'ll find
+             books and content from Authors and
              Readers you follow.`,
     }
+    this.handleClose = this.handleClose.bind(this)
+  }
+
+  componentWillMount = () => {
+    this.setState({
+      isOpen: true
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      isOpen: false,
+    })
   }
 
   render() {
-    return (
-      <div className='text-center'>
-        <h2>{this.state.title}</h2>
-        <p>{this.state.body}</p>
-      </div>
-    )
+    if (this.state.isOpen) {
+      return (
+        <Card className='readfeed-announcement'>
+          <img
+            src='./image/close.png'
+            className='general-font readfeed-announcement-close'
+            onClick={this.handleClose}
+          />
+          <CardText>
+            <h2>{this.state.title}</h2>
+            <p>{this.state.body}</p>
+          </CardText>
+        </Card>
+      )
+    }
+    return null
   }
 }
 
