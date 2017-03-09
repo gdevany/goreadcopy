2413341deae55bd4191b28779011c5bde1aed4d9
Carlos Sampol <sampol.90@gmail.com>
2017-03-09 12:39:50 -0500

    Adding profile image upload

--- src/app/components/readFeed/tiles/ProfileDetailerTile.js
+++ src/app/components/readFeed/tiles/ProfileDetailerTile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Images } from '../../../redux/actions'
+import Dropzone from 'react-dropzone'
 import {
   Card,
   CardText,
@@ -7,10 +9,37 @@ import {
 } from 'material-ui'
 import { Colors } from '../../../constants/style'
 
+const { uploadImage } = Images
+
 class ProfileDetailerTile extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      profileImageUpload: null,
+      hasProfileImage: false,
+    }
+  }
+
+  profileUpload = (file) => {
+    this.setState({ profileImageUpload: file })
+    this.setState({ hasProfileImage: false })
+
+    this.getBase64AndUpdate(file[0], 'profileImage')
+  }
+
+  getBase64AndUpdate = (file, imageType) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = () => resolve(reader.result)
+      reader.onerror = (error) => reject(`Error in getBase64: ${error}`)
+    }).then(res => this.props.uploadImage({ imageType, file: res }))
+  }
 
   render() {
     const { loginCount } = this.props
+
     if (loginCount) {
       return (
         <Card className='profile-detailer-container'>
@@ -27,20 +56,25 @@ class ProfileDetailerTile extends Component {
           </div>
           <CardText className='profile-detailer-content'>
             <div className='profile-detailer-actions-content'>
-              <a href='' className='profile-detailer-single-action'>
-                <div className='action-icon'>
-                  <span>+</span>
-                </div>
-                <div className='action-content'>
-                  <spam className='action-name'>Add Profile Photo</spam>
-                  <div className='litcoin-earn-container'>
-                    <span className='litcoin-amount'>
-                      3,500
-                    </span>
-                    <img className='litcoin-image' src='./image/litcoin.png' />
+              <Dropzone
+                onDrop={this.profileUpload}
+                className='dropzone-profile-detailer'
+              >
+                <a href='#' className='profile-detailer-single-action'>
+                  <div className='action-icon'>
+                    <span>+</span>
                   </div>
-                </div>
-              </a>
+                  <div className='action-content'>
+                    <spam className='action-name'>Add Profile Photo</spam>
+                    <div className='litcoin-earn-container'>
+                      <span className='litcoin-amount'>
+                        3,500
+                      </span>
+                      <img className='litcoin-image' src='./image/litcoin.png' />
+                    </div>
+                  </div>
+                </a>
+              </Dropzone>
               <a href='' className='profile-detailer-single-action'>
                 <div className='action-icon'>
                   <span>+</span>
@@ -69,20 +103,6 @@ class ProfileDetailerTile extends Component {
                   </div>
                 </div>
               </a>
-              <a href='' className='profile-detailer-single-action'>
-                <div className='action-icon'>
-                  <span>+</span>
-                </div>
-                <div className='action-content'>
-                  <spam className='action-name'>Add Profile Photo</spam>
-                  <div className='litcoin-earn-container'>
-                    <span className='litcoin-amount'>
-                      3,500
-                    </span>
-                    <img className='litcoin-image' src='./image/litcoin.png' />
-                  </div>
-                </div>
-              </a>
             </div>
             <a href='' className='profile-detailer-goto'>
               See your profile >
@@ -96,8 +116,9 @@ class ProfileDetailerTile extends Component {
 }
 const mapStateToProps = (state) => {
   return {
-    loginCount: state.currentReader.loginCount
+    loginCount: state.currentReader.loginCount,
+    profileImage: state.currentReader.profileImage
   }
 }
 
-export default connect(mapStateToProps, null)(ProfileDetailerTile)
+export default connect(mapStateToProps, { uploadImage })(ProfileDetailerTile)
