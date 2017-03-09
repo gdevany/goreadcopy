2334190c474eb38dec7da0ebf7d18ba23f6da011
Carlos Sampol <sampol.90@gmail.com>
2017-03-09 12:50:26 -0500

    Removing ProfileDetailer component and related styles

--- src/app/components/readFeed/MiddleContainer.js
+++ src/app/components/readFeed/MiddleContainer.js
@@ -1,12 +1,11 @@
 import React, { PureComponent } from 'react'
 import TilesWrapper from './TilesWrapper'
-import { AnnouncementTile, ProfileDetailerTile } from './tiles'
+import { AnnouncementTile } from './tiles'
 
 class MiddleContainer extends PureComponent {
   render() {
     return (
       <div className='middle-container small-12 large-6 columns'>
-        <ProfileDetailerTile />
         <AnnouncementTile/>
         <TilesWrapper />
       </div>
