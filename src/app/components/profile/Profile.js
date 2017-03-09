cb93deabfbf03b45ef60df1c8266a926559b722e
Mashadim <masha@philosophie.is>
2017-03-08 18:18:55 -0800

    setup initial profile page
    * Added suggestions

---
+++ src/app/components/profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LeftProfileContainer from './LeftProfileContainer'
+import RightProfileContainer from './RightProfileContainer'
+
+const Profile = () => {
+  return (
+    <div className='row'>
+      <LeftProfileContainer />
+      <RightProfileContainer />
+    </div>
+  )
+}
+
+export default Profile
---
+++ src/app/components/profile/RightProfileContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightProfileContainer = () => {
+  return (
+    <div className='right-container small-8 columns'>
+      RIGHT CONTAINER
+    </div>
+  )
+}
+
+export default RightProfileContainer
