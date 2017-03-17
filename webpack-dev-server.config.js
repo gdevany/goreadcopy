7b526df3f7c3191f4c4f1fdc19ec810db11e5d3c
Emir Salazar <emir@nextcenturypublishing.com>
2017-03-17 15:09:17 -0500

    Removing unused dependencies from the project.

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -91,10 +91,6 @@ module.exports = {
       {
         test: /\.svg$/,
         loader: "url?limit=10000&mimetype=image/svg+xml"
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
       }
     ],
   }
