b61e2381931308dd917b82f970f48678406f3680
Carlos Sampol <sampol.90@gmail.com>
2017-03-19 19:06:22 -0500

    Addigng styles and structure to first settigns tab

--- src/app/components/settings/SettingsTabs.js
+++ src/app/components/settings/SettingsTabs.js
@@ -1,5 +1,23 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
+import PrimaryButton from '../common/PrimaryButton'
+import Toggle from 'material-ui/Toggle'
+import { Colors } from '../../constants/style'
+
+const styles = {
+  lableStyle: {
+    fontSize: 18,
+  },
+  elementStyle: {
+    padding: '1em 0',
+  },
+  thumbSwitched: {
+    backgroundColor: Colors.blue,
+  },
+  trackSwitched: {
+    backgroundColor: '#d3ebfb',
+  },
+}
 
 class SettingsTabs extends PureComponent {
   constructor(props) {
@@ -10,6 +28,8 @@ class SettingsTabs extends PureComponent {
       tabTwoOpen: false,
       tabThreeOpen: false,
     }
+
+    this.handleButtonClick = this.handleButtonClick.bind(this)
   }
 
   handleTabClick = (tabIndex) => {
@@ -34,6 +54,137 @@ class SettingsTabs extends PureComponent {
         tabThreeOpen: true,
       })
     }
+  }
+
+  handleButtonClick = (event) => {
+    event.preventDefault()
+    console.log('Click on ImageUpload')
+  }
+
+  renderTabOne = () => {
+    const { currentReader } = this.props
+    return (
+      <article className='settings-single-tab-content small-12 columns small-centered'>
+        <div className='settings-section-title-container'>
+          <h3 className='settings-section-title'>Edit my Profile</h3>
+        </div>
+        <div className='profile-photo-editor-container'>
+          <h4 className='profile-photo-title' >Profile Photo</h4>
+          <div className='profile-foto-actions-container'>
+            <figure className='profile-photo-editor-figure'>
+              <img src={currentReader.profileImage}/>
+            </figure>
+            <PrimaryButton
+              label='+ Upload'
+              onClick={this.handleButtonClick}
+            />
+          </div>
+        </div>
+        <div className='profile-photo-editor-container'>
+          <h4 className='profile-photo-title' >Cover Photo</h4>
+          <div className='profile-foto-actions-container'>
+            <figure className='background-photo-editor-figure'>
+              <img src={currentReader.backgroundImage}/>
+            </figure>
+            <PrimaryButton
+              label='+ Upload'
+              onClick={this.handleButtonClick}
+            />
+          </div>
+        </div>
+        <div className='profile-editor-favorite-genres'>
+          <h4 className='profile-editor-favorite-genres-title'>
+            Favorite genres
+          </h4>
+          <a onClick={this.handleButtonClick}>
+            + Add or edit genres
+          </a>
+        </div>
+        <div className='profile-editor-favorite-quotes'>
+          <h4 className='profile-editor-favorite-quotes-title'>
+            Favorite Quoutes
+          </h4>
+          <div className='profile-edidor-quote-container'>
+            <h5 className='profile-editor-favorite-quotes-num'>
+              Favorite Quoute #1
+            </h5>
+            <textarea className='profile-editor-favorite-quote-textarea'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat null
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </textarea>
+          </div>
+          <a
+            onClick={this.handleButtonClick}
+          >
+            + Add favorite quoute
+          </a>
+        </div>
+        <div className='profile-editor-socialmedia-accounts'>
+          <h4 className='profile-editor-favorite-socialmedia-title'>
+            Show my social media accounts
+          </h4>
+          <div className='editor-favorite-socialmedia-container'>
+            <Toggle
+              label='Facebook'
+              labelPosition='left'
+              labelStyle={styles.lableStyle}
+              style={styles.elementStyle}
+              thumbSwitchedStyle={styles.thumbSwitched}
+              trackSwitchedStyle={styles.trackSwitched}
+            />
+            <Toggle
+              label='Twitter'
+              labelPosition='left'
+              labelStyle={styles.lableStyle}
+              style={styles.elementStyle}
+              thumbSwitchedStyle={styles.thumbSwitched}
+              trackSwitchedStyle={styles.trackSwitched}
+            />
+            <Toggle
+              label='Linkedin'
+              labelPosition='left'
+              labelStyle={styles.lableStyle}
+              style={styles.elementStyle}
+              thumbSwitchedStyle={styles.thumbSwitched}
+              trackSwitchedStyle={styles.trackSwitched}
+            />
+            <Toggle
+              label='Google +'
+              labelPosition='left'
+              labelStyle={styles.lableStyle}
+              style={styles.elementStyle}
+              thumbSwitchedStyle={styles.thumbSwitched}
+              trackSwitchedStyle={styles.trackSwitched}
+            />
+          </div>
+          <PrimaryButton
+            label='Save'
+            onClick={this.handleButtonClick}
+          />
+        </div>
+      </article>
+    )
+  }
+
+  renderTabTwo = () => {
+    return (
+      <article className='settings-single-tab-content small-12 columns small-centered'>
+        Tab 2
+      </article>
+    )
+  }
+
+  renderTabThree = () => {
+    return (
+      <article className='settings-single-tab-content small-12 columns small-centered'>
+        Tab 3
+      </article>
+    )
   }
 
   render() {
@@ -75,27 +226,9 @@ class SettingsTabs extends PureComponent {
           </ul>
         </section>
         <section className='settings-main-content row'>
-          {tabOneOpen ?
-            (
-              <article className='settings-single-tab-content small-12 columns'>
-                Tab 1
-              </article>
-            ) : null
-          }
-          {tabTwoOpen ?
-            (
-              <article className='settings-single-tab-content small-12 columns'>
-                Tab 2
-              </article>
-            ) : null
-          }
-          {tabThreeOpen ?
-            (
-              <article className='settings-single-tab-content small-12 columns'>
-                Tab 3
-              </article>
-            ) : null
-          }
+          {tabOneOpen ? this.renderTabOne() : null}
+          {tabTwoOpen ? this.renderTabTwo() : null}
+          {tabThreeOpen ? this.renderTabThree() : null}
         </section>
       </section>
     )
