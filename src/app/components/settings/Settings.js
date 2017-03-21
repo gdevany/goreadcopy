23a34dbce1379c71bc6259ac88b0d294147faa7c
Carlos Sampol <sampol.90@gmail.com>
2017-03-21 14:06:44 -0500

    Adding validation to Settings Page logged out

--- src/app/components/settings/Settings.js
+++ src/app/components/settings/Settings.js
@@ -1,25 +1,52 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import NavMenu from '../common/NavMenu'
+import Home from '../home//Home'
 import SettingsTabs from './SettingsTabs'
 import { CurrentReader } from '../../redux/actions'
 import { Auth } from '../../services'
+import { Auth as AuthAction } from '../../redux/actions'
+import { Auth as CurrentToken } from '../../services'
 
 const { getCurrentReader } = CurrentReader
+const { verifyUserToken } = AuthAction
 
 class Settings extends PureComponent {
 
-  componentWillMount = () => this.props.getCurrentReader()
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount = () => {
+
+    const token = CurrentToken.token()
+    if (token) {
+      this.props.verifyUserToken({
+        token,
+      })
+    }
+
+    this.props.getCurrentReader()
+
+  }
+  componentWillReceiveProps = (nextProps) => {
+    if (nextProps.currentReader.token) {
+      this.props.getCurrentReader()
+    }
+  }
 
   render() {
     const isUserLoggedIn = Auth.currentUserExists()
-    return (
+    return CurrentToken.currentUserExists() ?
+    (
       <div>
         <NavMenu isUserLoggedIn={isUserLoggedIn}/>
         <div className='settings-page'>
           <SettingsTabs/>
         </div>
       </div>
+    ) : (
+      <Home />
     )
   }
 }
@@ -29,4 +56,4 @@ const mapStateToProps = (state) => {
     currentReader: state.currentReader
   }
 }
-export default connect(mapStateToProps, { getCurrentReader })(Settings)
+export default connect(mapStateToProps, { getCurrentReader, verifyUserToken })(Settings)
