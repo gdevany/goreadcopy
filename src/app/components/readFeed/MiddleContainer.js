5dbcc8d4b3d181dbee48b6ea19e4b92acd28b75a
Emir Salazar <emir@nextcenturypublishing.com>
2017-03-24 08:16:48 -0500

    Adding statuspost on readfeed and adding limit to 10000 chars.

--- src/app/components/readFeed/MiddleContainer.js
+++ src/app/components/readFeed/MiddleContainer.js
@@ -3,17 +3,19 @@ import { connect } from 'react-redux'
 import { Tiles } from '../../redux/actions'
 import TilesWrapper from './TilesWrapper'
 import { AnnouncementTile } from './tiles'
+import { StatusPost } from '../common'
 
-const { getReadFeedTiles } = Tiles
+const { getReadFeedTiles, prependReadFeedTile } = Tiles
 
 class MiddleContainer extends PureComponent {
   componentWillMount = () => this.props.getReadFeedTiles()
 
   render() {
-    const { readFeed } = this.props
+    const { readFeed, userId, prependReadFeedTile } = this.props
     return (
       <div className='middle-container small-12 large-6 columns'>
         <AnnouncementTile/>
+        <StatusPost targetId={userId} postNewTile={prependReadFeedTile}/>
         {readFeed ? <TilesWrapper feed={readFeed}/> : null}
       </div>
     )
@@ -26,4 +28,7 @@ const mapStateToProps = ({
   }
 }) => { return { readFeed } }
 
-export default connect(mapStateToProps, { getReadFeedTiles })(MiddleContainer)
+export default connect(mapStateToProps, {
+  getReadFeedTiles,
+  prependReadFeedTile
+})(MiddleContainer)
