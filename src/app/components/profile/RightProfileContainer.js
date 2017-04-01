7d992b08044af752caddd7cdb1a5faf9391d17ca
Carlos Sampol <sampol.90@gmail.com>
2017-03-31 20:59:43 -0500

    Right sidebar proportions

--- src/app/components/profile/RightProfileContainer.js
+++ src/app/components/profile/RightProfileContainer.js
@@ -4,7 +4,7 @@ import SidebarAdWrapper from '../common/SidebarAdWrapper'
 class RightProfileContainer extends PureComponent {
   render() {
     return (
-      <div className='right-container small-12 medium-12 large-3 columns'>
+      <div className='right-profile-container small-12 medium-12 large-3 columns'>
         <SidebarAdWrapper />
       </div>
     )
