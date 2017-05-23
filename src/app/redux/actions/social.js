6bea9f6322fd59d0b62a970c9e7f598aa8ad1732
Carlos Sampol <sampol.90@gmail.com>
2017-05-23 10:05:17 -0500

    Updating currenReader on share

--- src/app/redux/actions/social.js
+++ src/app/redux/actions/social.js
@@ -1,8 +1,10 @@
 import { Social } from '../../services/api'
+import { getCurrentReader } from './currentReader'
 
 export function sharePost(data, contentId) {
   return dispatch => {
     Social.sharePost(contentId, data)
+      .then(res => dispatch(getCurrentReader()))
       .catch(err => console.error(`Error in sharePost ${err}`))
   }
 }
