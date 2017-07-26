1616248077715b1fac7226af7e769813ea13cd8f
Joshua Zafrani <joshua@nextcenturypublishing.com>
2017-07-26 11:42:40 -0500

    Fixes profile css soo ads looks more like readfeed

--- src/app/components/common/SidebarAd.js
+++ src/app/components/common/SidebarAd.js
@@ -2,11 +2,9 @@ import React, { PureComponent } from 'react'
 
 class SidebarAd extends PureComponent {
   render() {
-    const { content, isProfileAd, adClass } = this.props
+    const { content, adClass } = this.props
     return (
-      <div style={adClass} className={isProfileAd ?
-        'base-tile-sidebar-container' : 'base-tile-sidebar-container box'}
-      >
+      <div style={adClass} className={'base-tile-sidebar-container box'}>
         <div className='adv-sidebar-container'>
           <a href={content.isExternalTarget ? content.targetUrl : content.url}>
             <figure className='heading-overflow-figure'>
