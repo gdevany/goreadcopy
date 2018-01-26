9f698b8e564acabf847018f27491f46dd946f666
Rolando Perez <me@rolilink.com>
2018-01-26 16:14:52 -0500

    added extract-text plugin to bundle css into a css file, modified the webpack transfer plugin rules to not transfer src files into the build folder

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -104,6 +104,7 @@ module.exports = {
        return module.context && module.context.indexOf('node_modules') !== -1;
       },
     }),
+    new ExtractTextPlugin({filename: '[name].css', allChunks: true}),
     //--------------------------------------------------
     // Only enable when you want to use the Analyzer!!!!
     //--------------------------------------------------
