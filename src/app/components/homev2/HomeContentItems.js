d52ffc5765f3a7a747442f58c5884eb79335fedd
Emir Salazar <emir@goread.com>
2018-04-24 13:38:43 -0500

    Adding placeholders for books and other minor improvements.

---
+++ src/app/components/homev2/HomeContentItems.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
+import { Link } from 'react-router';
+
+export function Ad({ item: { id, classes, image, url, alt, isLink, target }}) {
+  return isLink ? (
+    <Link to={url} target={target}>
+      <img className={classes} src={image} alt={alt} />
+    </Link>
+  ) : (
+    <a href={url} target={target}>
+      <img className={classes} src={image} alt={alt} />
+    </a>
+  );
+}
+
+export function Hero({ items }) {
+  return (
+    <div className="hero section-spacing">
+      <UncontrolledCarousel items={items} />
+    </div>
+  );
+}
+
+export function HomeHero({ items }) {
+  return (
+    <div className="hero-wrapper">
+      <div className="d-block d-sm-none">
+        <Hero items={items} />
+      </div>
+      <div className="d-none d-sm-block">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Hero items={items} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function HomeAds({ ads }) {
+  return (
+    <div className="container">
+      <div className="row section-spacing">
+        <div className="col-sm-8">
+          <Ad item={ads[0]} />
+        </div>
+        <div className="col-sm-4">
+          <Ad item={ads[1]} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function HomeImageLinks({ ads }) {
+  return (
+    <div className="container">
+      <div className="row section-spacing">
+        <div className="col">
+          <Ad item={ads[2]} />
+        </div>
+      </div>
+      <div className="row section-spacing">
+        <div className="col-sm-6">
+          <Ad item={ads[3]} />
+        </div>
+        <div className="col-sm-6">
+          <Ad item={ads[4]} />
+        </div>
+      </div>
+    </div>
+  );
+}
