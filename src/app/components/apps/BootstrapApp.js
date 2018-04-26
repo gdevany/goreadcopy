8b4b91cf799bc6e1dd1cddd97a771ab08fb2cb4e
Emir Salazar <emir@goread.com>
2018-04-26 13:16:30 -0500

    Fixing GOR-34 with change in production ruleset for handling SCSS/CSS files.

--- src/app/components/apps/BootstrapApp.js
+++ src/app/components/apps/BootstrapApp.js
@@ -1,54 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { StyleRoot } from 'radium';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../../client/styles/app/views/home/index.scss';
 
 class BootstrapApp extends Component {
   static propTypes = {
     children: PropTypes.node,
   }
 
-  static childContextTypes = {
-    muiTheme: PropTypes.object,
-  }
-
   static contextTypes = {
     router: PropTypes.object.isRequired,
-  }
-
-  getChildContext() {
-    return {
-      muiTheme: this.state.muiTheme,
-    };
-  }
-
-  componentWillMount() {
-    this.setState({
-      muiTheme: getMuiTheme(),
-    });
-  }
-
-  componentWillReceiveProps(nextProps, nextContext) {
-    const newMuiTheme = nextContext.muiTheme ? nextContext.muiTheme : this.state.muiTheme;
-    this.setState({
-      muiTheme: newMuiTheme,
-    });
   }
 
   render() {
     return (
       <div className="app BootstrapCSS">
-        <StyleRoot>
-          { React.cloneElement(this.props.children) }
-        </StyleRoot>
+        { React.cloneElement(this.props.children) }
       </div>
     );
   }
 }
-
-BootstrapApp.childContextTypes = {
-  muiTheme: PropTypes.object.isRequired,
-};
 
 export default BootstrapApp;
