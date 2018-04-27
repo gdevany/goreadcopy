7a08c99600b3e5612992efc23446620ef95f12f2
Emir Salazar <emir@goread.com>
2018-04-27 16:24:33 -0500

    JIRA/GOR-20: Adding Bootstrap's Collapsable utility to Footer Column's lists for Re-designed Home Page.

--- src/app/components/common/footer/MainFooter.js
+++ src/app/components/common/footer/MainFooter.js
@@ -4,45 +4,59 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faArrowUp from '@fortawesome/fontawesome-free-solid/faArrowUp';
 import FooterColumn from './FooterColumn';
 
-const GetToKnowUs = [
-  { id: 0, isLink: false, action: "#", text: "About" },
-  { id: 1, isLink: false, action: "#", text: "Careers" },
-  { id: 2, isLink: false, action: "#", text: "Investor Relations" },
-  { id: 3, isLink: false, action: "#", text: "Our Mission" },
-];
-
-const MakeMoneyWithUs = [
-  { id: 0, isLink: false, action: "#", text: "GoRead for Authors" },
-  { id: 1, isLink: false, action: "#", text: "Sell Your Books" },
-  { id: 2, isLink: false, action: "#", text: "Publish Your Book" },
-  { id: 3, isLink: false, action: "#", text: "Promote Your Business" },
-  { id: 4, isLink: false, action: "#", text: "Promote Your Products" },
-];
-
-const GoReadChildren = [
-  { id: 0, isLink: false, action: "#", text: "Help Us Help Kids" },
-];
-
-const LetUsHelpYou = [
-  { id: 0, isLink: false, action: "#", text: "Your Account" },
-  { id: 1, isLink: false, action: "#", text: "Your Orders" },
-  { id: 2, isLink: false, action: "#", text: "Returns" },
-  { id: 3, isLink: false, action: "#", text: "Help" },
+const FooterColumns = [
+  {
+    key: 1,
+    title: 'Get to Know Us',
+    items: [
+      { id: 0, isLink: false, action: '#', text: 'About' },
+      { id: 1, isLink: false, action: '#', text: 'Careers' },
+      { id: 2, isLink: false, action: '#', text: 'Investor Relations' },
+      { id: 3, isLink: false, action: '#', text: 'Our Mission' },
+    ],
+  },
+  {
+    key: 2,
+    title: 'Make Money with Us',
+    items: [
+      { id: 0, isLink: false, action: '#', text: 'GoRead for Authors' },
+      { id: 1, isLink: false, action: '#', text: 'Sell Your Books' },
+      { id: 2, isLink: false, action: '#', text: 'Publish Your Book' },
+      { id: 3, isLink: false, action: '#', text: 'Promote Your Business' },
+      { id: 4, isLink: false, action: '#', text: 'Promote Your Products' },
+    ],
+  },
+  {
+    key: 3,
+    title: 'GoRead Children\'s Literacy Fund',
+    items: [
+      { id: 0, isLink: false, action: '#', text: 'Help Us Help Kids' },
+    ],
+  },
+  {
+    key: 4,
+    title: 'Let Us Help You',
+    items: [
+      { id: 0, isLink: false, action: '#', text: 'Your Account' },
+      { id: 1, isLink: false, action: '#', text: 'Your Orders' },
+      { id: 2, isLink: false, action: '#', text: 'Returns' },
+      { id: 3, isLink: false, action: '#', text: 'Help' },
+    ],
+  },
 ];
 
 const MainFooter = props => (
   <div className="footer d-flex flex-column justify-content-center flex-sm-row justify-content-sm-around">
-    <FooterColumn header="Stay in the Know">
-      <span>Sign up for savings, news and updates.</span>
-      <form action="#" className="form-inline newsletter">
-        <input type="text" className="form-control mb-2" placeholder="Enter your e-mail address" />
-        <button type="submit" className="btn mb-2">GO</button>
-      </form>
+    <FooterColumn title="Stay in the Know">
+      <div className="footer-form">
+        <span>Sign up for savings, news and updates.</span>
+        <form action="#" className="form-inline newsletter">
+          <input type="text" className="form-control mb-2" placeholder="Enter your e-mail address" />
+          <button type="submit" className="btn mb-2">GO</button>
+        </form>
+      </div>
     </FooterColumn>
-    <FooterColumn header="Get to Know Us" list={GetToKnowUs} />
-    <FooterColumn header="Make Money with Us" list={MakeMoneyWithUs} />
-    <FooterColumn header="GoRead Children's Literacy Fund" list={GoReadChildren} />
-    <FooterColumn header="Let Us Help You" list={LetUsHelpYou} />
+    { FooterColumns.map(column => <FooterColumn {...column} />) }
     <div className="footer-action-scroll d-none d-sm-block">
       <FontAwesomeIcon onClick={() => scroller.scrollToTop()} icon={faArrowUp} size="lg" />
     </div>
