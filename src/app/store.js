3b59dd2085b4c237d4d41fd839d73ba80bec29c5
Rolando Perez <me@rolilink.com>
2018-04-30 22:07:57 -0500

    added articles redux state logic

--- src/app/store.js
+++ src/app/store.js
@@ -1,25 +1,32 @@
-import { applyMiddleware, compose, createStore } from 'redux'
-import { routerMiddleware } from 'react-router-redux'
-import { browserHistory } from 'react-router'
-import initialState from './initialState'
-import reducers from './redux/reducers/rootReducer'
-import thunk from 'redux-thunk'
+import { applyMiddleware, compose, createStore } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import PromiseMiddleware from 'redux-promise-middleware';
+import initialState from './initialState';
+import reducers from './redux/reducers/rootReducer';
 
-const routeMiddleware = routerMiddleware(browserHistory)
-let composeEnhancers
+
+const routeMiddleware = routerMiddleware(browserHistory);
+let composeEnhancers;
+const middlewares = [
+  thunk,
+  routeMiddleware,
+  new PromiseMiddleware(),
+];
 
 if (process.env.NODE_ENV === 'production') {
-  composeEnhancers = compose
+  composeEnhancers = compose;
 } else {
-  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  middlewares.push(logger);
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
-const store = createStore(reducers, initialState,
-  composeEnhancers(
-    applyMiddleware(
-      routeMiddleware,
-      thunk,
-    )
-  )
-)
 
-export default store
+const store = createStore(
+  reducers,
+  initialState,
+  composeEnhancers(applyMiddleware(...middlewares)),
+);
+
+export default store;
