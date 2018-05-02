1ec64f491b9a25951ebc79d03f3ee247b3eedb17
Rolando Perez <me@rolilink.com>
2018-05-02 17:09:33 -0500

    added state, reducers, actions and selectors for new releases

--- src/app/redux/reducers/books.js
+++ src/app/redux/reducers/books.js
@@ -14,6 +14,13 @@ const initialState = {
     perPage: 10,
     page: 0,
   },
+  newReleases: {
+    isFetching: false,
+    byIds: [],
+    count: null,
+    perPage: 10,
+    page: 0,
+  },
 };
 
 export const normalizeEntities = entitites => (entitites.reduce((normalizedEntities, entity) => ({
@@ -22,7 +29,7 @@ export const normalizeEntities = entitites => (entitites.reduce((normalizedEntit
 }), {}));
 
 const entitiesReducer = handleAction(
-  combineActions(A.GET_BEST_SELLERS_FULFILLED),
+  combineActions(A.GET_BEST_SELLERS_FULFILLED, A.GET_NEW_RELEASES_FULFILLED),
   (state, { payload }) => ({
     ...state,
     ...normalizeEntities(payload.books),
@@ -62,10 +69,27 @@ export const bestSellersReducer = handleActions({
   [A.RESET_BEST_SELLERS]: () => initialState.bestSellers,
 }, initialState.bestSellers);
 
+export const newReleasesReducer = handleActions({
+  [A.GET_NEW_RELEASES_PENDING]: state => ({
+    ...state,
+    isFetching: true,
+  }),
+  [A.GET_NEW_RELEASES_FULFILLED]: (state, { payload }) => ({
+    ...state,
+    byIds: [...state.byIds, ...payload.books.map(book => book.id)],
+    perPage: payload.perPage,
+    page: payload.page,
+    count: payload.count,
+    isFetching: false,
+  }),
+  [A.RESET_NEW_RELEASES]: () => initialState.newReleases,
+}, initialState.newReleases);
+
 
 export default combineReducers({
   entities: entitiesReducer,
   payload: payloadReducer,
   isFetching: isFetchingReducer,
   bestSellers: bestSellersReducer,
+  newReleases: newReleasesReducer,
 });
