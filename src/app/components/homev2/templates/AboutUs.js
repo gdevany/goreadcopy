44ca7d368da435970a43557a67f4f1c60e8c82e2
Joshua Zafrani <joshua@nextcenturypublishing.com>
2018-05-08 15:58:23 -0500

    Blank template

---
+++ src/app/components/homev2/templates/AboutUs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { MainNavView } from '../../views';
+
+class AboutUs extends Component {
+  render() {
+    return (
+      <MainNavView>
+        <div className="testTemplate" />
+      </MainNavView>
+    );
+  }
+}
+
+export default AboutUs;
--- src/app/routes.js
+++ src/app/routes.js
@@ -21,6 +21,7 @@ import { PublicWrapper } from './components/public';
 import { ReferralHome } from './components/referral';
 import { Literacy } from './components/literacy';
 import { Settings } from './components/settings';
+import AboutUs from './components/homev2/templates/AboutUs';
 import { Auth } from './services';
 
 const isUserLoggedIn = Auth.currentUserExists();
@@ -63,6 +64,7 @@ const Routes = (
     <Route path="/privacy" context="privacy" component={PublicWrapper} />
     <Route path="/accounts/signup" component={SignUpModal} />
     <Route path="/accounts/login" component={LogInModal} />
+    <Route path="/aboutus" component={useBootstrapFor(AboutUs)} />
   </Route>
 );
 
