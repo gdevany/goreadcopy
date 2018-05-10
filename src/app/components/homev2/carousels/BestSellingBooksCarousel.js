4476e3f7e1b9b22527afdcc65d40c8f690fbe0eb
Rolando Perez <me@rolilink.com>
2018-05-10 16:08:29 -0500

    books links connected to redux

--- src/app/components/homev2/carousels/BestSellingBooksCarousel.js
+++ src/app/components/homev2/carousels/BestSellingBooksCarousel.js
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import { withBooksWithPagination } from '../hocs';
-import { getBestSellers } from '../../../redux/actions/books';
+import { getBestSellers, resetBestSellers } from '../../../redux/actions/books';
 import { getBestSellers as bestSellersSelector } from '../../../redux/selectors/books';
 import BookCarousel from '../BookCarousel';
 
 
 const mapDispatchToProps = {
   fetchBooks: getBestSellers,
+  resetBooks: resetBestSellers,
 };
 
 const mapStateToProps = (state) => {
