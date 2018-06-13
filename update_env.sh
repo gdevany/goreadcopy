92e5722e599e34212393d41bd852e77b11327ecb
Alexandre Matos <alexandre@xmatos.com>
2018-06-13 19:44:23 -0300

    small fixes

--- update_env.sh
+++ update_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Usage: update_env.sh COMMIT GIT_DIR
-# Ex: update_env.sh master ~/src/goread-frontend
+# Ex: update_env.sh master goread-frontend
+
+set -e
 
 if [ -z $1 ]; then
     COMMIT="master"
@@ -20,40 +22,46 @@ else
     DEPLOY_NAME="$COMMIT"
 fi
 
+if [ ! -d "$GIT_DIR" ]; then
+    mkdir "$GIT_DIR"
+    CLONE_REPO=1
+fi
+
+cd "$GIT_DIR" || exit 1
+
 function fetch_code {
-    if [ ! -d "$GIT_DIR" ]; then
+    if [[ -v CLONE_REPO ]]; then
         echo "cloning repo"
-        git clone git@gitlab.readerslegacy.com:RL/goread-frontend.git "$GIT_DIR"
+        git clone git@gitlab.readerslegacy.com:RL/goread-frontend.git .
     else
-        echo "fetching repo"
+        echo "fetching $COMMIT"
         git fetch origin "$COMMIT"
+        git checkout "$COMMIT"
         git reset --hard "origin/$COMMIT"
     fi
 }
 
 function install_packages {
-    yarn --cwd "$GIT_DIR" install
+    yarn install
 }
 
 function build_bundle {
-    INIT_CWD="$GIT_DIR" npm run build:"$DEPLOY_NAME"
+    npm run build:"$DEPLOY_NAME"
 }
 
 function fix_static_hash {
     index_file="index.html"
-    code_dir="$GIT_DIR/public/"
     bundle_files=("vendor" "main" "runtime")
     exts=("js" "css")
-    
     hex_hash=$(echo $(date +"%s") | md5sum | awk '{print $1}')
 
-    cd "$code_dir" || exit 1
+    cd "public" || exit 1
 
     for ext in "${exts[@]}"; do
         for bundle_file in "${bundle_files[@]}"; do
-            if [ "$bundle_file" != "runtime" || "$ext" != "css" ]; then
+            if [[ "$bundle_file" != "runtime" || "$ext" != "css" ]]; then
                 file_to_find="$bundle_file.*.$ext"
-                file_to_change=$(ls -t "$file_to_find" | head -1)
+                file_to_change=$(ls -t $file_to_find | head -1)
 
                 if [ -e "$file_to_change" ]; then
                     new_file_name="$bundle_file.$hex_hash.$ext"
@@ -64,7 +72,7 @@ function fix_static_hash {
                 if [ "$bundle_file" != "runtime" ]; then
                     gzip_ext="$ext.gz"
                     gzip_file_to_find="$bundle_file.*.$gzip_ext"
-                    gzip_file_to_change=$(ls -t "$gzip_file_to_find" | head -1)
+                    gzip_file_to_change=$(ls -t $gzip_file_to_find | head -1)
 
                     if [ -e "$gzip_file_to_change" ]; then
                         gzip_new_file_name="$bundle_file.$hex_hash.$gzip_ext"
@@ -73,7 +81,7 @@ function fix_static_hash {
                 fi
 
                 #Cleaning old bundle files
-                find "$bundle_file*.$ext*" \
+                find $bundle_file*.$ext* \
                      ! -iname "$new_file_name" ! -iname "$gzip_new_file_name" \
                      -delete
             fi
@@ -82,11 +90,11 @@ function fix_static_hash {
     
     directory_to_find="icons-"
     new_directory_name="$directory_to_find$hex_hash"
-    directory_to_change=$(ls -dt "$directory_to_find*" | head -1)
+    directory_to_change=$(ls -dt $directory_to_find* | head -1)
 
     mv "$directory_to_change" "$new_directory_name"
     sed -i -- 's/$directory_to_change/$new_directory_name/g' "$index_file"
-    find "$directory_to_find*" -type d ! -iname "$new_directory_name" -delete
+    find $directory_to_find* -type d ! -iname "$new_directory_name" -delete
 }
 
 function restart_express {
@@ -94,10 +102,10 @@ function restart_express {
     sudo supervisorctl start express:*
 }
 
-fetch_code
+# fetch_code
 # install_packages
 # build_bundle
-# fix_static_hash
+fix_static_hash
 # restart_express
 
 exit 0
