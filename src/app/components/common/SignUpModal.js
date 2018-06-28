2837f641dad30183d662ed7ac2857142674e75f2
Emir Salazar <emir@goread.com>
2018-06-28 12:29:52 -0500

    GOR#114: Adding FB Pixel for SignUp page.

--- src/app/components/common/SignUpModal.js
+++ src/app/components/common/SignUpModal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import R from 'ramda';
 import { connect } from 'react-redux';
@@ -121,6 +122,32 @@ class SignUpModal extends Component {
 
     return (
       <BaseNavView>
+        <Helmet>
+          <script>
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '952697181431277');
+              fbq('track', 'CompleteRegistration');
+            `}
+          </script>
+          <noscript>
+            {`
+              <img
+                height="1"
+                width="1"
+                style="display:none"
+                src="https://www.facebook.com/tr?id=952697181431277&ev=PageView&noscript=1"
+              />
+            `}
+          </noscript>
+        </Helmet>
         <div className="row">
           <h1 className="center-text large-header top-container">
             Join GoRead
