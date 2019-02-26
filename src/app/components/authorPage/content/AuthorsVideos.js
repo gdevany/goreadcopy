9e367f252e5d3fd7f8c793f7d69bcce415876100
Greg Devany <greg.devany@yahoo.com>
2019-02-26 12:09:54 -0600

    WIP: GOR-152 Cleanup code, styles, padding, margins, etc

--- src/app/components/authorPage/content/AuthorsVideos.js
+++ src/app/components/authorPage/content/AuthorsVideos.js
@@ -1,150 +1,11 @@
 import React, { PureComponent } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router";
 import ReactPlayer from "react-player";
-import { Colors } from "../../../constants/style";
-import { Card, CardActions } from "material-ui";
 
 //NOTE !! : AuthorPageContent (switch) default changed to AuhorsPageVideos
 //NOTE !! : Change default back to AuthorPageWall
 
-// TODO: restructure once new endpoint completed
-// TODO: cleanup and move styles to scss
-//   -note: may need some of this unused styles once restructured
-//   -note: code for like/comment/share pulled in from TileDefault.js
+// NOTE: code for like/comment/share pulled in from TileDefault.js
 // TODO: convert icons for like/comment/share from react-player to material-ui
-
-const styles = {
-  popover: {
-    borderRadius: 9,
-    boxShadow: "rgba(0, 0, 0, 0.1) 0px 2px 20px"
-  },
-  headerContainer: {
-    padding: 30,
-    textAlign: "left"
-  },
-  commentHeaderContainer: {
-    padding: "30px 30px 15px",
-    textAlign: "left"
-  },
-  card: {
-    boxShadow: "rgba(0, 0, 0, 0.1) 0px 2px 20px"
-  },
-  nameText: {
-    color: Colors.blue,
-    display: "inline",
-    fontSize: 14,
-    fontWeight: 600
-  },
-  postText: {
-    color: Colors.black,
-    display: "inline",
-    marginLeft: 6,
-    marginRight: 0
-  },
-  commentTimeStamp: {
-    display: "inline",
-    marginLeft: 10
-  },
-  timeStamp: {
-    fontSize: 14,
-    color: Colors.grey,
-    position: "absolute",
-    left: 85,
-    top: 53
-  },
-  textContainer: {
-    marginTop: -5,
-    paddingRight: 40
-  },
-  contentContainer: {
-    padding: "0 30px 30px 30px"
-  },
-  socialContainer: {
-    margin: 0
-  },
-  socialWrapper: {
-    borderTop: "2px solid #F4F4F4",
-    fontSize: 14,
-    opacity: "100",
-    padding: "20px 30px"
-  },
-  commentIconContainer: {
-    textAlign: "center"
-  },
-  commentContainer: {
-    borderTop: "2px solid #F4F4F4",
-    opacity: "100",
-    padding: 0
-  },
-  commentActions: {
-    borderBottom: "2px solid #F4F4F4",
-    fontSize: 14,
-    opacity: "100",
-    padding: "10px 20px"
-  },
-  likesContainer: {
-    textAlign: "left",
-    padding: 0
-  },
-  shareContainer: {
-    textAlign: "right",
-    padding: 0
-  },
-  sharePopover: {
-    margin: 0,
-    padding: 20,
-    listStyle: "none"
-  },
-  shareLink: {
-    marginBottom: 10
-  },
-  shareButton: {
-    margin: "0 auto"
-  },
-  shareGoReadLink: {
-    margin: 0
-  },
-  commentCard: {
-    boxShadow: "none"
-  },
-  commentContent: {
-    padding: "0 30px",
-    marginLeft: 54,
-    textAlign: "justify"
-  },
-  postButton: {
-    float: "right",
-    marginRight: 35
-  },
-  cardContainer: {
-    borderRadius: 5,
-    boxShadow: "rgba(222, 222, 222, 0.5) 0px 4px 20px 0px"
-  },
-  postInput: {
-    border: "1px solid #F4F4F4",
-    borderRadius: 3,
-    opacity: "100",
-    outline: "none",
-    marginLeft: 85,
-    maxWidth: 450
-  },
-  childCommentContainer: {
-    marginLeft: 40
-  },
-  childCommentActionContainer: {
-    textAlign: "right",
-    padding: "0px 10px"
-  },
-  childCommentHeaderContainer: {
-    padding: "20px 20px 15px",
-    textAlign: "left"
-  },
-  refresh: {
-    display: "inline-block",
-    position: "relative"
-  }
-};
 
 const temps = {
   videos: [
@@ -188,12 +49,29 @@ class AuthorVideos extends PureComponent {
       VideoDescLimit: 140,
       liked: 0,
       likedCount: 0,
-      userLogged: true,
+      isUserLoggedIn: true,
       commented: true,
       commentedCount: 0,
-      sharedCount: 0
+      sharedCount: 0,
+      openModal: false
     };
   }
+
+  renderAddVideoButton = () => {
+    const allowUserToAdd = this.state.isUserLoggedIn && (
+      <reactFragment>
+        <button
+          className="authors-videos-addVideoButton"
+          onClick={e => {
+            this.handleModalOpen(e);
+          }}
+        >
+          Add Video
+        </button>
+      </reactFragment>
+    );
+    return allowUserToAdd;
+  };
 
   renderVideos = () => {
     let videos = temps.videos.map(video => {
@@ -203,39 +81,32 @@ class AuthorVideos extends PureComponent {
             <ReactPlayer
               className="authors-videos-player"
               url={video.videoURL}
-              light={true}
+              // light={true}
             />
           </div>
-          <h5 className="authors-videos-title">
-            <strong>{video.title}</strong>
-          </h5>
+          <div className="authors-videos-title">{video.title}</div>
           <p className="authors-videos-desc">
-            {this.renderVideoDesc(
-              video.desc,
-              this.state.VideoDescLimit,
-              video.id
-            )}
+            {this.renderVideoDesc(video, this.state.VideoDescLimit)}
           </p>
-          <div className="authors-videos-like-share-box">
-            {this.renderLikesCommentsShare()}
-          </div>
+          <div className="authors-videos-likeShareBoxBorder" />
+          <div>{this.renderLikesCommentsShare()}</div>
         </div>
       );
     });
     return videos;
   };
 
-  renderVideoDesc = (text, limit, id) => {
+  renderVideoDesc = (video, limit) => {
     const { isDescTrunced, videoIdClicked } = this.state;
-
-    return text.length < limit ? (
-      text
+    const { desc, id } = video;
+    return desc.length < limit ? (
+      desc
     ) : (
       <reactFragment>
         <span className="authors-page-lighten">
           {videoIdClicked === id && isDescTrunced !== true
-            ? text
-            : this.truncInfo(text, limit)}
+            ? desc
+            : this.truncInfo(desc, limit)}
         </span>
         <a
           className="authors-videos-a-span"
@@ -262,64 +133,61 @@ class AuthorVideos extends PureComponent {
       sharedCount
     } = this.state;
     return (
-      <Card>
-        <CardActions style={styles.socialWrapper}>
-          <div style={styles.socialContainer} className="row">
-            <div className="small-4 columns" style={styles.likesContainer}>
-              <div className="likes-count">
-                <a
-                  onClick={
-                    this.state.userLogged //TODO
-                      ? this.handleLiked
-                      : // : this.handleLogInModalOpen
-                        null // TODO
-                  }
-                  className={liked ? "liked" : "not-liked"}
-                />
+      <div className="authors-videos-socialWrapper row">
+        <div className="small-4 columns text-left">
+          <div className="likes-count">
+            <a
+              onClick={
+                this.state.isUserLoggedIn //TODO
+                  ? this.handleLiked
+                  : // : this.handleLogInModalOpen
+                    null // TODO
+              }
+              className={liked ? "liked" : "not-liked"}
+            />
 
-                <span className={liked ? "liked-number" : "not-liked-number"}>
-                  {likedCount}
-                </span>
-              </div>
-            </div>
-            <div
-              className="small-4 columns"
-              style={styles.commentIconContainer}
-            >
-              <div className="comments-count">
-                <a
-                  onClick={
-                    this.state.userLogged
-                      ? // ? this.handleCommentsOpen
-                        // : this.handleLogInModalOpen
-                        null // TODO
-                      : null // TODO
-                  }
-                  className={commented ? "commented" : "not-commented"}
-                />
-
-                <span
-                  className={
-                    commented ? "commented-number" : "not-commented-number"
-                  }
-                >
-                  {commentedCount}
-                </span>
-              </div>
-            </div>
-            <div className="small-4 columns" style={styles.shareContainer}>
-              <div className="shared-count">
-                {/* <a onClick={this.handleShareOpen}> */}
-                <span className="share" ref="share">
-                  Share {sharedCount}
-                </span>
-                {/* </a> */}
-              </div>
-            </div>
+            <span className={liked ? "liked-number" : "not-liked-number"}>
+              {likedCount}
+            </span>
           </div>
-        </CardActions>
-      </Card>
+        </div>
+        <div className="small-4 columns text-center">
+          <div className="comments-count">
+            <a
+              onClick={
+                this.state.isUserLoggedIn
+                  ? // ? this.handleCommentsOpen
+                    // : this.handleLogInModalOpen
+                    null // TODO
+                  : null // TODO
+              }
+              className={commented ? "commented" : "not-commented"}
+            />
+            <span
+              className={
+                commented ? "commented-number" : "not-commented-number"
+              }
+            >
+              {commentedCount}
+            </span>
+          </div>
+        </div>
+        <div className="small-4 columns text-right">
+          <div className="shared-count">
+            {/* <a onClick={this.handleShareOpen}> */}
+            <span className="share" ref="share">
+              Share {sharedCount}
+            </span>
+            {/* </a> */}
+          </div>
+        </div>
+      </div>
     );
+  };
+
+  handleModalOpen = e => {
+    e.preventDefault();
+    this.setState({ openModal: true });
   };
 
   handleReadMoreLess = id => {
@@ -349,7 +217,12 @@ class AuthorVideos extends PureComponent {
 
   render() {
     return (
-      <div className="authors-videos-wrapperBox">{this.renderVideos()}</div>
+      <reactFragment>
+        <div className="authors-videos-UserLoggedAddVideo-wrapper">
+          {this.renderAddVideoButton()}
+        </div>
+        <div className="authors-videos-wrapperBox">{this.renderVideos()}</div>
+      </reactFragment>
     );
   }
 }
